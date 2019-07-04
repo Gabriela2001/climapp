f7122324289ab7f16b0086cb7e10085aa0cc12a5
Gabriela2001 <rokagabriela@gmail.com>
2019-07-04 08:31:43 -0600

    arreglo de repetición

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,6 +10,8 @@ angular.module('starter.controllers', [])
 			url: "http://api.openweathermap.org/data/2.5/forecast?q="+y+",GT&APPID=f74006d77c6256a75842bc2ede1dda31"
 		}).then(function Datos(variable) {
 			console.log(variable)
+			$scope.Listado = [];
+			$scope.PaisIngresado="";
 			$scope.Listado.push(variable);
 		})
 	}
